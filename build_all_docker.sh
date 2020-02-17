31e49cf2c4e2c6affcb2035dde0094bc24849286
Brian Boerst <brian@boerst.com>
2020-02-16 17:34:52 -0800

    Adding tanner builds

--- build_all_docker.sh
+++ build_all_docker.sh
@@ -1,8 +1,19 @@
+# Build and push all top-level docker images
 for n in $(ls docker )
 do
     if test -f "docker/$n/Dockerfile"; then
         echo "Working on $n now"
         docker build -t bboerst/$n:latest docker/$n
+        docker push bboerst/$n:latest
+    fi
+done
+
+# Build and push all tanner docker images
+for n in $(ls docker/tanner )
+do
+    if test -f "docker/tanner/$n/Dockerfile"; then
+        echo "Working on $n now"
+        docker build -t bboerst/$n:latest docker/tanner/$n
         docker push bboerst/$n:latest
     fi
 done
